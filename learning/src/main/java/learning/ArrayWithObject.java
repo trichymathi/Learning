b741cac933b630988347a8bb0afa297b45f48949
mathimaran02 <mathimaran02@gmail.com>
2025-04-16 16:51:58 +0530

    New Changes

--- learning/src/main/java/learning/ArrayWithObject.java
+++ learning/src/main/java/learning/ArrayWithObject.java
@@ -5,6 +5,8 @@ package learning;
 public class ArrayWithObject {
 
 	public static void main(String[] args) {
+		
+		Object arr[] = {"hari",1,null};
 		String array[] = {"Dharshan", "Mathi", "Dharni kumar", "Mathi maraqn"};
 		int max = array[0].length();
 		String name ="";
---
+++ learning/src/main/java/learning/FindThirdLargestDigitInArray.java
@@ -0,0 +1,34 @@
+package learning;
+
+import java.util.Arrays;
+
+public class FindThirdLargestDigitInArray {
+
+	public static void main(String[] args) {
+		int arr[]= {80,70,60,50,40,90,100,200};
+		
+//		Arrays.sort(arr);
+//		
+//		int a =arr.length-3;
+//		
+//		System.out.println(arr[a]);
+		
+		int n=arr.length;
+		 
+		
+//bubble Sort Method
+		for(int i=0;i<n-1;i++) {
+			for(int j=0;j<n-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp =arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+		int a =arr.length-3;
+		System.out.println(arr[a]);
+	}
+
+}
