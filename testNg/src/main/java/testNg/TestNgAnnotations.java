5f3bf4d19d1b74d9222ed92d8215c4c4e31e5601
mathimaran02 <mathimaran02@gmail.com>
2025-05-09 09:50:52 +0530

    Changes New 01

--- testNg/src/main/java/testNg/TestNgAnnotations.java
+++ testNg/src/main/java/testNg/TestNgAnnotations.java
@@ -1,5 +1,6 @@
 package testNg;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestNgAnnotations {
@@ -9,4 +10,22 @@ public class TestNgAnnotations {
 		System.out.println("Test 01");
 	}
 
+	
+	@Test(dataProvider = "loginData")
+	public void dataProviderName(String userName,String password) {
+		System.out.println(userName);
+		System.out.println(password);
+	}
+	
+	
+	
+	
+	@DataProvider(parallel = true,indices = {0,2})
+	public String[][] loginData(){
+		String name[][]= {{"username1","password1"},
+				{"username2","password2"},
+				{"username3","password3"}
+		};
+		return name;	
+	}
 }
--- testNg/src/main/java/testNg/ToUpperCase.java
+++ testNg/src/main/java/testNg/ToUpperCase.java
@@ -3,17 +3,19 @@ package testNg;
 public class ToUpperCase {
 
 	public static void main(String[] args) {
-		 String name ="compile and run your Java code online";
+		 String name ="compile and run your java code online";
 //		 String value =name.toUpperCase();
 //		 System.out.println(value);
 	        String[] split=name.split(" ");
 	        String upperCaseName="";
 	        for(String a:split){
 	            String upperWord ="";
+	            char[] charArray = a.toCharArray();
 	            for(int i=0;i<a.length();i++){
 	                if(i==1){
-	                    char form =a.charAt(i);
-	                    char character=Character.toUpperCase(form);
+//	                    char form =a.charAt(i);
+//	                    char character=Character.toUpperCase(form);
+	                	char character=Character.toUpperCase(charArray[i]);
 	                    upperWord=upperWord+character;
 	                }else{
 	                    upperWord=upperWord+a.charAt(i);
