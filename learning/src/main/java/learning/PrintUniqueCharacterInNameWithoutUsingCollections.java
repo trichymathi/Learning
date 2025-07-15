7b19b6cf742ebd13234395aca700c67f09bbb395
Mathimaran Rajendran <Mathimaran Rajendran@DESKTOP-BRU56L1>
2025-07-15 17:08:16 +0530

    Line Formating

--- learning/src/main/java/learning/PrintUniqueCharacterInNameWithoutUsingCollections.java
+++ learning/src/main/java/learning/PrintUniqueCharacterInNameWithoutUsingCollections.java
@@ -7,21 +7,21 @@ public class PrintUniqueCharacterInNameWithoutUsingCollections {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter Your Name");
-		String name=scan.nextLine();
-		char[] ch=name.toLowerCase().toCharArray();
-		for(int i=0;i<ch.length;i++) {
-			int count =1;
-			if(ch[i]=='0') {
+		String name = scan.nextLine();
+		char[] ch = name.toLowerCase().toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			int count = 1;
+			if (ch[i] == '0') {
 				continue;
 			}
-			for(int j=i+1;j<ch.length;j++) {
-				if(ch[i]==ch[j]) {
+			for (int j = i + 1; j < ch.length; j++) {
+				if (ch[i] == ch[j]) {
 					count++;
-					ch[j]='0';
+					ch[j] = '0';
 				}
 			}
-			if(count==1) {
-		     System.out.println(ch[i]+" : "+count);
+			if (count == 1) {
+				System.out.println(ch[i] + " : " + count);
 			}
 		}
 	}
